039aec4d35968f26d786a9624f49f46a902064ca
Halina Tulchii <halendeihalyna@gmail.com>
2023-05-08 09:32:20 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+// import pokemonLoading from '../images/pokemon-loading.png'
+// import { ToastContainer } from 'react-toastify';
+import PokemonForm from './PokemonForm';
+import css from './App.module.css'
+
+export default class App extends Component {
+  state = {
+    pokemonName: '',
+
 };
+
+  handleFormSubmit = pokemonName => {
+    this.setState({pokemonName});
+  }
+
+
+  render() {
+    return (
+      <div className={css.App__box}>
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+      </div>
+    )
+  }
+  
+};
+
+
+
+
+
+
+
+// Simple way
+
+// state = {
+//   pokemon: null,
+//   loading: false,
+// }
+
+// componentDidMount() {
+//   this.setState({ loading: true });
+//   setTimeout(() => {
+//     fetch('https://pokeapi.co/api/v2/pokemon/meowth')
+//     .then(res => res.json())
+//       .then(pokemon => this.setState({ pokemon }))
+//       .finally(() => this.setState({ loading: false }));
+//   }, 1000)
+
+// }
+
+// render() {
+//   return (
+//     <div>
+//       {this.state.loading && <div><h1>Loading...</h1>
+//         <img src={pokemonLoading} width={450}/></div>}
+//       {this.state.pokemon && <div>{this.state.pokemon.name}</div>}
+//     </div>
+//   );
+// }
