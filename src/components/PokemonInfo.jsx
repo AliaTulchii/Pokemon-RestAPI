b4ac5a29086f57db2ab347e37ce9b453cd7e76ec
Halina Tulchii <halendeihalyna@gmail.com>
2023-05-09 12:38:14 +0300

    update

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import loading from '../images/loading.png';
+import loadingPokemon from '../images/loadingPokemon.png';
 import pokemonLoading from '../images/pokemonLoading.png';
 import css from './PokemonInfo.module.css'
 // import PokemonDataView from './PokemonDataView';
@@ -9,13 +9,13 @@ import css from './PokemonInfo.module.css'
 export default class PokemonInfo extends Component {
     state = {
         pokemon: null,
-        loading:false,
+        loading: false,
     }
 
 
 
     componentDidUpdate(prevProps, prevState) {
-        if (prevProps.pokemonName !== this.props.pokemonInfo) {
+        if (prevProps.pokemonName !== this.props.pokemonName) {
             console.log('Pokemons name changed');
             
             this.setState({ loading: true });
@@ -25,7 +25,7 @@ export default class PokemonInfo extends Component {
                 .then(res => res.json())
                 .then(pokemon => this.setState({ pokemon }))
                 .finally(() => this.setState({loading: false}));
-            },2000)
+            },1000)
             
            
                   
@@ -34,12 +34,26 @@ export default class PokemonInfo extends Component {
 
 
     render() {
+        
         return <div>
             <h1 className={css.Info__title}>Pokemon Info</h1>
-
-            {this.state.loading && <div> <img src={pokemonLoading} alt="pokemon Loading" width={300}/> <h2>Wait a second we search your pokemon...</h2></div>}
-            {!this.props.pokemonName && <div className={css.Info__box}><img src={loading} alt="not found" width={300} /> <h2 className={css.Info__text}>Let's see who is inside</h2> </div>}
-            {this.state.pokemon && <div>{this.state.pokemon.name}</div>}
+            <div>
+            {this.state.loading && <div className={css.Info__loadingBox}> <img src={pokemonLoading} alt="pokemon Loading" width={300} className={css.Info__loadingImg} /> <h2>Wait a second we search your pokemon...</h2></div>}
+            {!this.props.pokemonName && <div className={css.Info__box}><img src={loadingPokemon} alt="not found" width={300} /> <h2 className={css.Info__text}>Let's see who is inside</h2> </div>}
+                {this.state.pokemon && (
+                    <div className={css.Info__card}>
+                        <img
+                            src={this.state.pokemon.sprites.other['official-artwork'].front_default}
+                            alt=""
+                            width={300}
+                            className={css.Info__pokemonImg}
+                        />
+                        <div><h3 className={css.Info_name}>{this.state.pokemon.name}</h3></div>
+                        
+                        
+                    </div>)}
+            </div>
+           
         </div>
     }
 }
